7528325e848f9342cf02bbb22df69c334522b091
vekshan <vekshan@gmail.com>
2020-06-23 01:02:42 -0400

    Added code for header specification

--- packet_receiver.py
+++ packet_receiver.py
@@ -4,7 +4,7 @@ import socket
 
 HEADER_SIZE = 40
 SERVER = socket.gethostname()
-PORT = 9999
+PORT = 1234
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         msg =''
         receiving = True
         while True:
-            data = conn.recv(HEADER_SIZE)
+            data = conn.recv(HEADER_SIZE)#should be either message size or header size to fix
             if receiving:
                 print(f'new message length:{msg[:HEADER_SIZE]}')
             if not data:
--- packet_sender.py
+++ packet_sender.py
@@ -1,13 +1,15 @@
 import binascii
 import socket
 
-
+HEADER_SIZE = 40
 SERVER = socket.gethostbyname(socket.gethostname())
-PORT = 9999
+PORT = 1234
 
-
+HEADER = '450000281c46400040069D35C0A80003C0A80001'
+msg = "works"
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERVER, PORT))
+    msg = f'{len(msg):<{HEADER_SIZE}}'+ msg
     s.sendall(b'Hello, world')
     
     
