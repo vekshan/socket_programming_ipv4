229e7b01f7dd2bc0785c3cc2b6879bb06fa3f4c0
vekshan <vekshan@gmail.com>
2020-06-23 02:35:14 -0400

    Header and message read properly in hex

--- packet_receiver.py
+++ packet_receiver.py
@@ -12,11 +12,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, add = s.accept()
     with conn:
-        msg =''
+        msg = ''
         receiving = True
+        msg_length = 0
         while True:
-            data = conn.recv(HEADER_SIZE)#should be either message size or header size to fix
+            # should be either message size or header size to fix
+            data = conn.recv(HEADER_SIZE)
             if receiving:
-                print(f'new message length:{msg[:HEADER_SIZE]}')
-            if not data:
+                msg_length = int(data[4:8], 16)
+                print(f'new message length:{msg_length}')
+                receiving = False
+            msg += data.decode("utf-8")
+
+            if len(msg)-HEADER_SIZE == msg_length:
+                print(msg)
                 break
--- packet_sender.py
+++ packet_sender.py
@@ -5,11 +5,12 @@ HEADER_SIZE = 40
 SERVER = socket.gethostbyname(socket.gethostname())
 PORT = 1234
 
-HEADER = '450000281c46400040069D35C0A80003C0A80001'
-msg = "works"
+HEADER = '4500 0028 1c46 4000 4006 9D35 C0A8 0003 C0A8 0001'.replace(' ', '')
+
+msg = '434f 4c4f 4d42 4941 2032 202d 204d 4553 5349 2030'.replace(' ', '')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERVER, PORT))
-    msg = f'{len(msg):<{HEADER_SIZE}}'+ msg
-    s.sendall(b'Hello, world')
+    msg = HEADER + msg
+    s.send(bytes(msg,"utf-8"))
     
     
