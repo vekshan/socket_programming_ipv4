2889bdb947cf15e5a7850e0c26341cf222593884
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-06-26 14:10:51 -0400

    [New] Added conversion for IP address to hex

--- packet_receiver.py
+++ packet_receiver.py
@@ -12,7 +12,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, add = s.accept()
     with conn:
-        msg = ''
+        msg = ""
         receiving = True
         msg_length = 0
         while True:
@@ -20,10 +20,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(HEADER_SIZE)
             if receiving:
                 msg_length = int(data[4:8], 16)
-                print(f'new message length:{msg_length}')
+                print(f"new message length:{msg_length}")
                 receiving = False
             msg += data.decode("utf-8")
 
-            if len(msg)-HEADER_SIZE == msg_length:
+            if len(msg) - HEADER_SIZE == msg_length:
                 print(msg)
                 break
--- packet_sender.py
+++ packet_sender.py
@@ -1,9 +1,11 @@
 import binascii
 import socket
 
+
 def checksum():
-    #TODO
+    # TODO
     pass
+
 
 ######### MAIN ###########
 HEADER_SIZE = 40
@@ -12,24 +14,28 @@ PORT = 1234
 
 
 # TODO get user input
-msg = ''
+msg = ""
 
-HEADER = bytes('4500 0028 1c46 4000 4006 0000 C0A8 0003 C0A8 0001'.replace(' ', ''),'utf-8') # for testing only, should be done below
+HEADER = bytes(
+    "4500 0028 1c46 4000 4006 0000 C0A8 0003 C0A8 0001".replace(" ", ""), "utf-8"
+)  # for testing only, should be done below
 
-msg = 'COLOMBIA 2 - MESSI 0'
+msg = "COLOMBIA 2 - MESSI 0"
 
 # encode msg
-msg = binascii.hexlify(bytes(msg, 'utf-8'))
+msg = binascii.hexlify(bytes(msg, "utf-8"))
 
 # calculate total length of IP header, payload + 20 for header, convert to hex
-msg_length = '{:04X}'.format(len(msg) + 20 )
+msg_length = "{:04X}".format(len(msg) + 20)
 
 # convert ip addresses to hex
+print("You IP Address is: " + SERVER)
+IPHex = binascii.hexlify(socket.inet_aton(SERVER)).upper()
 
 # calculate checksum in hex
 
-# concatenate header and convert to bytes
 
+# concatenate header and convert to bytes
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
